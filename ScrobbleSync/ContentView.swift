85d3a85714386eff9a8d0d442a675b1644bfa078
mattbolanos <matthew.a.bolanos@gmail.com>
2025-12-13 12:09:15 -0500

    first commit

--- ScrobbleSync/ContentView.swift
+++ ScrobbleSync/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  ScrobbleSync
-//
-//  Created by Matt Bolaños on 12/13/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -13,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello world!")
         }
         .padding()
     }
