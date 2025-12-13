0dba6981d38061983d72552a32839250a5b3e2f9
mattbolanos <matthew.a.bolanos@gmail.com>
2025-12-13 11:58:25 -0500

    Initial Commit

---
+++ ScrobbleSync/ScrobbleSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrobbleSyncApp.swift
+//  ScrobbleSync
+//
+//  Created by Matt Bolaños on 12/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrobbleSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
